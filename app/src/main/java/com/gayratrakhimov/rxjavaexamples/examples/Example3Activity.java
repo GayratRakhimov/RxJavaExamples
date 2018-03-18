6efe93a1bb4dc74c012653530eb4032ab74db4b4
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-18 13:03:57 +0500

    - Added new example activity for Observable methods

---
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/Example3Activity.java
@@ -0,0 +1,19 @@
+package com.gayratrakhimov.rxjavaexamples.examples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gayratrakhimov.rxjavaexamples.R;
+
+public class Example3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example3);
+
+        // Observable methods
+
+    }
+
+}

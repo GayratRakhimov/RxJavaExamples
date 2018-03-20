b0022d9c56bb92d61f908b19023a42fc70611452
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-20 14:59:14 +0500

    - Moved Observable Observer example to introduction module

--- app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/introduction/ObservableObserverActivity.java
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/introduction/ObservableObserverActivity.java
@@ -1,4 +1,4 @@
-package com.gayratrakhimov.rxjavaexamples.examples.observable;
+package com.gayratrakhimov.rxjavaexamples.examples.introduction;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;

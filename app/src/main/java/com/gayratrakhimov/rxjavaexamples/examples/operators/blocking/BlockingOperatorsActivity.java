915d2f2175ffecab1d29ccad3ae526ff44220769
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-27 21:37:55 +0500

    Added forEach example

--- app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/operators/blocking/BlockingOperatorsActivity.java
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/operators/blocking/BlockingOperatorsActivity.java
@@ -1,7 +1,11 @@
-package com.gayratrakhimov.rxjavaexamples.examples.operators;
+package com.gayratrakhimov.rxjavaexamples.examples.operators.blocking;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class BlockingOperatorsActivity extends AppCompatActivity {
 
@@ -9,9 +13,16 @@ public class BlockingOperatorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // *** BLOCKING ***
+        Observable observable1 = Observable.just(5,3,4,2);
         
         // forEach
+        observable1.forEach(new Consumer() {
+            @Override
+            public void accept(Object o) throws Exception {
+                Log.d("RxJavaTag", "accept:"+o);
+            }
+        });
+
         // first
         // firstOrDefault
         // last

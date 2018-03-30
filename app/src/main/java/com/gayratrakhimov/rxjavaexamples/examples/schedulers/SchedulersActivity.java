8f75def0bb58d9b0e76fd47ce24d78d38e56798f
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-30 20:13:05 +0500

    Added io scheduler example

--- app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/schedulers/SchedulersActivity.java
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/schedulers/SchedulersActivity.java
@@ -1,13 +1,48 @@
 package com.gayratrakhimov.rxjavaexamples.examples.schedulers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class SchedulersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Observable observable = Observable.just(5,3,4,2);
+
+        final Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("RxJavaTag", "onSubscribe");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.d("RxJavaTag", "onNext: " + o);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("RxJavaTag", "onError:" + e.toString());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("RxJavaTag", "onComplete");
+            }
+        };
+
+        observable = observable.subscribeOn(Schedulers.io());
+
+        observable.subscribe(observer);
+
     }
 
 }

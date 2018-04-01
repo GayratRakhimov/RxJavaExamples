cb3b31d97384501faae0a918799c12a490b8283b
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-04-01 12:05:07 +0500

    Added Disposable example

---
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/disposable/DisposableActivity.java
@@ -0,0 +1,61 @@
+package com.gayratrakhimov.rxjavaexamples.examples.disposable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.gayratrakhimov.rxjavaexamples.R;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+public class DisposableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disposable);
+
+        Observable observable =
+                Observable.just("Marshmallow", "Nougat", "Oreo")
+                        .delay(3, TimeUnit.SECONDS);
+
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("RxJavaTag", "onSubscribe");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.d("RxJavaTag", "onNext: "+o);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("RxJavaTag", "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("RxJavaTag", "onComplete");
+            }
+        };
+
+        // Observer subscribes to Observable to consume items
+        Disposable disposable = observable.subscribe(new Consumer() {
+            @Override
+            public void accept(Object o) throws Exception {
+                Log.d("RxJavaTag", "accept:"+o);
+            }
+        });
+
+        disposable.dispose();
+
+    }
+
+}

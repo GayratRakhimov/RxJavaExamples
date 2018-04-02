5ce13895d87dd41a134813169ca02a708b9360d3
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-04-02 22:06:27 +0500

    Added new Activity for lambda example

--- app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/subject/SubjectActivity.java
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/subject/SubjectActivity.java
@@ -3,13 +3,11 @@ package com.gayratrakhimov.rxjavaexamples.examples.subject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.gayratrakhimov.rxjavaexamples.R;
-
 public class SubjectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subject);
     }
+
 }

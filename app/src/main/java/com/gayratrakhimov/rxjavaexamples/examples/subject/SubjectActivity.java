09bfe3faf0117d11af847a07e9dddbae5beb1158
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-04-02 22:04:03 +0500

    Added new activity for Subject examples

---
+++ app/src/main/java/com/gayratrakhimov/rxjavaexamples/examples/subject/SubjectActivity.java
@@ -0,0 +1,15 @@
+package com.gayratrakhimov.rxjavaexamples.examples.subject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gayratrakhimov.rxjavaexamples.R;
+
+public class SubjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+    }
+}
